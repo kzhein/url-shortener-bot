0f0cd0544561541fbc4ccb8a52cbd1393f2881c4
kzhein <kyawzinhein6@gmail.com>
2021-04-24 00:17:27 +0630

    add express body parser

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,10 @@ bot.on('message', async msg => {
 
 const app = express();
 
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
 app.listen(process.env.PORT, () => {
   console.log(`Bot running on port ${process.env.PORT}...`);
 });
